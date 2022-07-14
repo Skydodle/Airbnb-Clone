71e394cdc00053c51db0bf358e2fcd0834926e9b
skydodle <skydodle@gmail.com>
2022-07-14 16:39:44 -0700

    carousel list

--- Airbnb/src/screens/SearchResultsMap/index.js
+++ Airbnb/src/screens/SearchResultsMap/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, useWindowDimensions} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
 import places from '../../../assets/data/feed';
@@ -9,6 +9,8 @@ import PostCarouselItem from '../../components/PostCarouselItem';
 
 const SearchResultsMap = props => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
+  const width = useWindowDimensions().width;
 
   return (
     <View style={{width: '100%', height: '100%'}}>
@@ -32,8 +34,16 @@ const SearchResultsMap = props => {
         ))}
       </MapView>
 
-      <View style={{position: 'absolute', bottom: 40}}>
-        <PostCarouselItem post={places[0]} />
+      <View style={{position: 'absolute', bottom: 10}}>
+        <FlatList
+          data={places}
+          renderItem={({item}) => <PostCarouselItem post={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={width - 60}
+          snapToAlignment={'center'}
+          decelerationRate={'fast'}
+        />
       </View>
     </View>
   );
