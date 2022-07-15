d1d6d53f7b246592148edae7cdcac4749d26f2ee
skydodle <skydodle@gmail.com>
2022-07-14 17:22:01 -0700

    map sync

--- Airbnb/src/screens/SearchResultsMap/index.js
+++ Airbnb/src/screens/SearchResultsMap/index.js
@@ -1,20 +1,51 @@
-import React, {useState} from 'react';
-import {View, Text, FlatList, useWindowDimensions} from 'react-native';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, FlatList, useWindowDimensions, viewableItems} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 import places from '../../../assets/data/feed';
 
 import CustomMarker from '../../components/CustomMarker';
 import PostCarouselItem from '../../components/PostCarouselItem';
 
-const SearchResultsMap = props => {
+const SearchResultsMap = () => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
 
+  const flatlist = useRef();
+  const map = useRef();
+
   const width = useWindowDimensions().width;
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id);
+    }
+  });
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+    const index = places.findIndex(place => place.id === selectedPlaceId);
+    flatlist.current.scrollToIndex({index: index});
+    // console.warn('scroll to' + selectedPlaceId);
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId]);
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         style={{width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -36,6 +67,7 @@ const SearchResultsMap = props => {
 
       <View style={{position: 'absolute', bottom: 10}}>
         <FlatList
+          ref={flatlist}
           data={places}
           renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
@@ -43,6 +75,8 @@ const SearchResultsMap = props => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
